2e0dd489d25d8f59e8093f19e9c6a0766655d800
Mateus Araujo <mateus_1542@hotmail.com>
2021-03-28 16:47:01 -0300

    feat(search-page): implements pagination @SearchPage

--- lib/modules/search/presenter/search/search_page.dart
+++ lib/modules/search/presenter/search/search_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'package:clean_architecture/modules/search/presenter/search/search_bloc.dart';
+import 'package:clean_architecture/modules/search/presenter/search/states/events.dart';
 import 'package:clean_architecture/modules/search/presenter/search/states/state.dart';
 
 class SearchPage extends StatefulWidget {
@@ -29,7 +30,7 @@ class _SearchPageState extends State<SearchPage> {
           Padding(
             padding: const EdgeInsets.only(right: 8, left: 8, top: 8),
             child: TextField(
-              onChanged: bloc.add,
+              onChanged: (value) => bloc.add(GetSearchEvent(value)),
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: "Search...",
@@ -60,28 +61,69 @@ class _SearchPageState extends State<SearchPage> {
                   );
                 }
 
-                final list = (state as SearchSuccess).list;
+                return NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification scrollNotification) {
+                    // print(scrollNotification.metrics.pixels);
+                    // print(scrollNotification.metrics.maxScrollExtent);
+                    if (!(state is SearchLoading) &&
+                        scrollNotification.metrics.pixels >=
+                            scrollNotification.metrics.maxScrollExtent - 300) {
+                      // bloc.add(IncrementPageEvent());
+                      return true;
+                    }
 
-                return ListView.builder(
-                  itemCount: list.length,
-                  itemBuilder: (_, id) {
-                    final item = list[id];
-
-                    return ListTile(
-                      leading: item.img == null
-                          ? Container()
-                          : CircleAvatar(
-                              backgroundImage: NetworkImage(item.img),
-                            ),
-                      title: Text(item.title ?? ""),
-                      subtitle: Text(item.content ?? ""),
-                    );
+                    return false;
                   },
+                  child: ListView.builder(
+                    itemCount: bloc.list.length + 1,
+                    itemBuilder: (_, index) {
+                      if (index < bloc.list.length) {
+                        final item = bloc.list[index];
+
+                        return ListTile(
+                          leading: item.img == null
+                              ? Container()
+                              : CircleAvatar(
+                                  backgroundImage: NetworkImage(item.img),
+                                ),
+                          title: Text(item.title ?? ""),
+                          subtitle: Text(item.content ?? ""),
+                        );
+                      } else {
+                        return _buildLoadMoreButton(
+                          isLoading: state is SearchLoadingMore,
+                          onPressed: () => bloc.add(IncrementPageEvent()),
+                        );
+                      }
+                    },
+                  ),
                 );
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildLoadMoreButton({bool isLoading, void Function() onPressed}) {
+    return Center(
+      heightFactor: 2,
+      child: ElevatedButton(
+        child: isLoading
+            ? SizedBox(
+                height: 25,
+                width: 25,
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.white,
+                  strokeWidth: 3,
+                ),
+              )
+            : Text('Load more'),
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(200, 40),
+        ),
       ),
     );
   }
